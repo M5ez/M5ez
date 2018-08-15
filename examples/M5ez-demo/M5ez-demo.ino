c0a4371034f0dfb82dfbcab3d3021e08a42066fc
Rop <rop@gonggri.jp>
2018-08-16 00:57:36 +0200

    somehow demo sketch got deleted...

---
+++ examples/M5ez-demo/M5ez-demo.ino
@@ -0,0 +1,177 @@
+#include <M5Stack.h>
+#include <M5ez.h>
+
+#include "images.h"
+
+#define MAIN_DECLARED
+
+void setup() {
+
+  m5.begin();
+
+}
+
+void loop() {
+  ezMenu mainmenu("Welcome to M5ez");
+  mainmenu.addItem("Flexible text menus", mainmenu_menus);
+  mainmenu.addItem("Image menus", mainmenu_image);
+  mainmenu.addItem("Neat messages", mainmenu_msgs);
+  mainmenu.addItem("Multi-function buttons", mainmenu_buttons);
+  mainmenu.addItem("3-button text entry", mainmenu_entry);
+  mainmenu.addItem("Built-in WiFi support", ezWifiMenu);
+  mainmenu.addItem("Updates via https", mainmenu_ota);
+  mainmenu.upOnFirst("last|up");
+  mainmenu.downOnLast("first|down");
+  mainmenu.run(); 
+}
+
+void mainmenu_menus() { 
+  ezMenu submenu("This is a sub menu");
+  submenu.txtSmall();
+  submenu.addItem("You can make small menus");
+  submenu.addItem("Or big ones");
+  submenu.addItem("(Like the Main menu)");
+  submenu.addItem("In this menu most options");
+  submenu.addItem("Do absolutely nothing");
+  submenu.addItem("They are only here");
+  submenu.addItem("To demonstrate that menus");
+  submenu.addItem("Can run off the screen");
+  submenu.addItem("And will just scroll");
+  submenu.addItem("And scroll");
+  submenu.addItem("And Scroll");
+  submenu.addItem("And Scroll even more");   
+  submenu.addItem("more | Learn more about menus", submenu_more);
+  submenu.addItem("Exit | Go back to main menu");
+  submenu.run();
+}
+
+void submenu_more() {
+  ez.drawHeader("A simple menu in code...");
+  ez.printLmargin(10);
+  ez.println("");
+  ez.println("ezMenu menu(\"Main menu\");");
+  ez.println("menu.addItem(\"Option 1\");");
+  ez.println("menu.addItem(\"Option 2\");");
+  ez.println("menu.addItem(\"Option 3\");");
+  ez.println("while ( menu.run() ) {");
+  ez.println("  if (menu.pick == 1) {");
+  ez.println("    ez.msgBox (\"One!\");");
+  ez.println("  }");
+  ez.println("}");
+  ez.waitForButtons("OK");
+
+  ezMenu fontmenu("Menus can change looks");
+  fontmenu.txtFont(&Satisfy_24);
+  fontmenu.addItem("Menus can use");
+  fontmenu.addItem("Various Fonts");
+  fontmenu.runOnce();
+
+  ezMenu delmenu("Menus are dynamic");
+  delmenu.txtSmall();
+  delmenu.addItem("You can delete items");
+  delmenu.addItem("While the menu runs");
+  delmenu.addItem("Delete me!");
+  delmenu.addItem("Delete me!");
+  delmenu.addItem("Delete me!");
+  delmenu.addItem("Exit | Go back" );
+  while (delmenu.runOnce()) {
+    if (delmenu.pickName() == "Delete me!") {
+        delmenu.deleteItem(delmenu.pick());
+    }
+  }
+}
+
+void mainmenu_image() {
+  ezMenu images;
+  images.imgBackground(TFT_BLACK);
+  images.imgFromTop(40);
+  images.imgCaptionFont(&FreeSansBold12pt7b);
+  images.imgCaptionLocation(TC_DATUM);
+  images.imgCaptionColor(TFT_WHITE);
+  images.imgCaptionMargins(10,10);
+  images.addItem(sysinfo_jpg, "System Information", sysInfo);
+  images.addItem(wifi_jpg, "WiFi Settings", ezWifiMenu);
+  images.addItem(about_jpg, "About M5ez", aboutM5ez);
+  images.addItem(sleep_jpg, "Power Off", powerOff);
+  images.addItem(return_jpg, "Back");
+  images.run();
+}
+    
+void mainmenu_msgs() {
+  String cr = (String)char(13);
+  ez.msgBox("You can show messages", "ez.msgBox shows text");
+  ez.msgBox("Looking the way you want", "In any font !", "OK", true, &FreeSerifBold24pt7b, TFT_RED);
+  ez.msgBox("More ez.msgBox", "Even multi-line messages where everything lines up and is kept in the middle of the screen");
+  ez.msgBox("Questions, questions...", "But can it also show any buttons you want?", "No # # Yes"); 
+  ez.textBox("And there's ez.textBox", "To present or compose longer word-wrapped texts, you can use the ez.textBox function." + cr + cr + "M5ez (pronounced \"M5 easy\") is a complete interface builder library for the M5Stack ESP32 system. It allows even novice programmers to create good looking interfaces. It comes with menus as text or as images, message boxes, very flexible button setup (including different length presses and multi-button functions), 3-button text input (you have to see it to believe it) and built-in Wifi support. Now you can concentrate on what your program does, and let M5ez worry about everything else.", true);
+}
+    
+void mainmenu_buttons() {
+  ez.drawHeader("Simple buttons...");
+  ez.printFont(&FreeSans12pt7b);
+  ez.printLmargin(20);
+  ez.println("");      
+  ez.println("You can have three buttons");
+  ez.println("with defined funtions.");
+  ez.drawButtons("One # Two # Done");
+  printButton();
+  ez.clearScreen();
+  ez.drawHeader("More functions...");
+  ez.printFont(&FreeSans12pt7b);
+  ez.printLmargin(20);
+  ez.println("");      
+  ez.println("But why stop there?");
+  ez.println("If you press a little longer");
+  ez.println("You access the functions");
+  ez.println("printed in cyan.");
+  ez.drawButtons("One # Two # Three # Four # Done #");
+  printButton();
+  ez.clearScreen();
+  ez.drawHeader("Two keys ...");
+  ez.printFont(&FreeSans12pt7b);
+  ez.printLmargin(20);     
+  ez.println("It gets even better...", 20, ez.canvasTop() + 10);
+  ez.println("The purple bar shows the");
+  ez.println("functions for key combis.");
+  ez.println("See if you can work it out...");
+  ez.drawButtons("One # Two # Three # Four # Five # Six # Seven # Eight # Done");
+  printButton();
+}
+
+void printButton(){
+  while (true) {
+    String btnpressed = ez.getButtons();
+    if (btnpressed == "Done") break;
+    if (btnpressed != "") {
+        m5.lcd.fillRect (0, ez.canvasBottom() - 45, TFT_W, 40, SCREEN_BGCOLOR); 
+        ez.print(btnpressed, 20, ez.canvasBottom() - 45, &FreeSansBold18pt7b, TFT_RED);
+    }
+  }
+}
+
+void mainmenu_entry() {
+  if (ez.msgBox("We're gonna enter text ... !", "Have you learned to use the buttons? Go there first if you haven't been there. Or hit 'Go' to see if you can enter your name.", "Back # # Go") == "Go") {
+    String your_name = ez.textInput();
+    ez.msgBox("Pfew...", "Hi " + your_name + "! | | Now that was a pain! But it is good enough for entering, say, a WPA key, or don't you think?");
+    ez.msgBox("Don't worry", "(You do get better with practice...)");
+  }
+}
+
+void mainmenu_ota() {
+  if (ez.msgBox("Get OTA_https demo", "This will replace the demo with a program that can then load the demo program again.", "Cancel#OK#") == "OK") {
+    ezProgressBar progress_bar("OTA update in progress", "Downloading ...", "Abort");
+    #include "raw_githubusercontent_com.h" // the root certificate is now in const char * root_cert
+    if (ez.update("https://raw.githubusercontent.com/ropg/M5ez/master/compiled_binaries/OTA_https.bin", root_cert, &progress_bar)) {
+      ez.msgBox("Over The Air updater", "OTA download successful. Reboot to new firmware", "Reboot");
+      ESP.restart();
+    } else {
+    }
+  }
+}
+
+void powerOff() { m5.powerOFF(); }
+
+void aboutM5ez() {
+  ez.msgBox("About M5ez", "M5ez was written by | Rop Gonggrijp | | https://github.com/ropg/M5ez");
+}
+
---
+++ examples/M5ez-demo/sysinfo.ino
@@ -0,0 +1,85 @@
+/* This example shows how to use M5ez to do simple display things. 
+ * The "#ifndef MAIN_DECLARED" part is there so that it can also 
+ * simply be added to the directory of another sketch. As long as
+ * it contains "#define MAIN_DECLARED", the sketches do not 
+ * conflict and the other program can simply call sysInfo() if it 
+ * wants to display system information.
+ * 
+ * The MAIN_DECLARED is used further down to determine whether
+ * the app has an exit button. After all: if it is running stand-
+ * alone there is nothing to exit to.
+*/
+
+#ifndef MAIN_DECLARED
+
+#include <M5Stack.h>
+#include <M5ez.h>
+
+void setup() {
+  m5.begin();
+  sysInfo();
+}
+
+void loop() {
+
+}
+
+String exit_button = "";
+
+#else
+
+String exit_button = "Exit";
+
+#endif  // #ifndef MAIN_DECLARED
+
+
+
+void sysInfo() {
+  sysInfoPage1();
+  while(true) {
+    String btn = ez.getButtons();
+    if (btn == "up") sysInfoPage1();
+    if (btn == "down") sysInfoPage2();
+    if (btn == "Exit") break;
+  }
+}
+
+#include <SPIFFS.h>
+
+void sysInfoPage1() {
+  const byte tab = 120;
+  ez.clearScreen();
+  ez.drawHeader("System Information   (1/2)");
+  ez.drawButtons("#" + exit_button + "#down");
+  ez.printFont(&FreeSans9pt7b);
+  ez.printLmargin(10);
+  ez.println("");
+  ez.print("CPU freq:");  ez.println(String(ESP.getCpuFreqMHz()) + " MHz", tab);
+  ez.print("CPU cores:");  ez.println("2", tab);    //   :)
+  ez.print("Chip rev.:");  ez.println(String(ESP.getChipRevision()), tab);
+  ez.print("Flash speed:");  ez.println(String(ESP.getFlashChipSpeed() / 1000000) + " MHz", tab);
+  ez.print("Flash size:");  ez.println(String(ESP.getFlashChipSize() / 1000000) + " MB", tab);
+  ez.print("ESP SDK v.:");  ez.println(String(ESP.getSdkVersion()), tab);
+}
+
+void sysInfoPage2() {
+  const String SD_Type[5] = {"NONE", "MMC", "SD", "SDHC", "UNKNOWN"};
+  const byte tab = 140;
+  ez.clearScreen();
+  ez.drawHeader("System Information   (2/2)");
+  ez.drawButtons("up#" + exit_button + "#");
+  ez.printFont(&FreeSans9pt7b);
+  ez.printLmargin(10);
+  ez.println("");
+  ez.print("Free RAM:");  ez.println(String((long)ESP.getFreeHeap()) + " bytes", tab);
+  ez.print("Min. free seen:");  ez.println(String((long)esp_get_minimum_free_heap_size()) + " bytes", tab);
+  const int sd_type = SD.cardType();
+  SPIFFS.begin();
+  ez.print("SPIFFS size:");  ez.println(String((long)SPIFFS.totalBytes()) + " bytes", tab);
+  ez.print("SPIFFS used:");  ez.println(String((long)SPIFFS.usedBytes()) + " bytes", tab);
+  ez.print("SD type:");  ez.println(SD_Type[sd_type], tab);
+  if (sd_type != 0) {
+    ez.print("SD size:");  ez.println(String((long)SD.cardSize()  / 1000000) + " MB", tab);
+    ez.print("SD used:");  ez.println(String((long)SD.usedBytes()  / 1000000) + " MB", tab);
+  }
+}
